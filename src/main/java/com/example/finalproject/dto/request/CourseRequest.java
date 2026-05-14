538d1bd4755bd3fe6026781b3529ea76b27dafc9
nurbolat <djumadilovnurik9@gmail.com>
2026-05-14 17:48:23 +0500

    add size constraints to course and lesson request DTOs

--- src/main/java/com/example/finalproject/dto/request/CourseRequest.java
+++ src/main/java/com/example/finalproject/dto/request/CourseRequest.java
@@ -2,14 +2,17 @@ package com.example.finalproject.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class CourseRequest {
 
     @NotBlank(message = "Title is required")
+    @Size(min = 3, max = 200, message = "Title must be 3-200 characters")
     private String title;
 
+    @Size(max = 2000, message = "Description must not exceed 2000 characters")
     private String description;
 
     @NotNull(message = "Category is required")
