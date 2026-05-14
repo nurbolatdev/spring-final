260cb9341b05e26e93ceb8169262b4cd9d119352
nurbolat <djumadilovnurik9@gmail.com>
2026-05-14 12:25:55 +0500

    init project

---
+++ src/main/java/com/example/finalproject/NurbolatDjumadilovCourseApplication.java
@@ -0,0 +1,15 @@
+package com.example.finalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@SpringBootApplication
+@EnableAsync
+public class NurbolatDjumadilovCourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NurbolatDjumadilovCourseApplication.class, args);
+    }
+
+}
